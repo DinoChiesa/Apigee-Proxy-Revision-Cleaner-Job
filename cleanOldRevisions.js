b8273e6d14b867b8dd5cb21fe670298da2566d78
DinoChiesa <dchiesa@google.com>
2025-09-18 18:07:10 -0700

    feat: remove both sharedflows AND proxies

--- cleanOldRevisions.js
+++ cleanOldRevisions.js
@@ -2,10 +2,10 @@
 /* jshint node:true, esversion:9, strict:implied */
 // cleanOldRevisions.js
 // ------------------------------------------------------------------
-// In Apigee, for all proxies or sharedflows in an org, remove all
+// In Apigee, for all proxies and sharedflows in an org, remove all
 // but the latest N revisions. (Never remove a deployed revision).
 //
-// Copyright 2017-2023 Google LLC.
+// Copyright 2017-2023,2025 Google LLC.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -19,9 +19,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 //
-// last saved: <2023-December-15 13:26:20>
+// last saved: <2025-September-18 18:06:35>
 
-import fetch from "node-fetch";
 import apigeejs from "apigee-edge-js";
 import Getopt from "node-getopt";
 import util from "util";
@@ -29,26 +28,30 @@ import pLimit from "p-limit";
 
 const common = apigeejs.utility,
   apigee = apigeejs.apigee,
-  version = "20231024-1632",
+  version = "20250918-1735",
   getopt = new Getopt(
     common.commonOptions.concat([
       [
         "R",
         "regexp=ARG",
-        "Optional. Cull only proxies with names matching this regexp."
+        "Optional. Cull only proxies with names matching this regexp.",
       ],
       [
         "K",
         "numToKeep=ARG",
-        "Required. Max number of revisions of each proxy to retain."
+        "Required. Max number of revisions of each proxy to retain.",
       ],
-      ["S", "sharedflows", "Optional. Cull only sharedflows, not apiproxies."],
+      [
+        "",
+        "collection=ARG",
+        "Optional. Collection to cull. Either sharedflows or apiproxies. Default: both",
+      ],
       [
         "",
         "magictoken",
-        "Optional. Obtain a magic token from http://metadata.google.internal"
-      ]
-    ])
+        "Optional. Obtain a magic token from http://metadata.google.internal",
+      ],
+    ]),
   ).bindHelp();
 
 const getMagicToken = async () => {
@@ -56,8 +59,8 @@ const getMagicToken = async () => {
     "http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/default/token",
     {
       method: "GET",
-      headers: { "Metadata-Flavor": "Google" }
-    }
+      headers: { "Metadata-Flavor": "Google" },
+    },
   );
   const data = await response.json();
   return data.access_token;
@@ -65,23 +68,22 @@ const getMagicToken = async () => {
 
 // ========================================================
 process.on("unhandledRejection", (r) =>
-  console.log("\n*** unhandled promise rejection: " + util.format(r))
+  console.log("\n*** unhandled promise rejection: " + util.format(r)),
 );
 
 const isDeployed = (result) =>
-  (result.environment && result.environment.length != 0) ||
-  result.deployments;
+  (result.environment && result.environment.length != 0) || result.deployments;
 
 const opt = getopt.parse(
   process.env.CMDARGS
     ? process.env.CMDARGS.split(new RegExp(" +", "g"))
-    : process.argv.slice(2)
+    : process.argv.slice(2),
 );
 
 if (opt.options.verbose) {
   console.log(
     `Apigee Proxy / Sharedflow revision cleaner tool, version: ${version}\n` +
-      `Node.js ${process.version}\n`
+      `Node.js ${process.version}\n`,
   );
   common.logWrite("start");
 }
@@ -102,50 +104,80 @@ async function examineRevisions(collection, name, revisions) {
     revisions.sort((a, b) => a - b);
 
     let reducer = (promise, c, _ix, orig) =>
-    promise.then( a => {
-      if (a.length >= orig.length - opt.options.numToKeep) {return a;}
-      const options = { name, revision:c };
-      return collection.getDeployments(options)
-        .then(result => {
+      promise.then((a) => {
+        if (a.length >= orig.length - opt.options.numToKeep) {
+          return a;
+        }
+        const options = { name, revision: c };
+        return collection.getDeployments(options).then((result) => {
           if (opt.options.verbose) {
             common.logWrite(
               "deployments (%s r%s): %s",
               name,
               c,
-              JSON.stringify(result)
+              JSON.stringify(result),
             );
           }
           return isDeployed(result) ? a : [...a, c];
         });
-    });
+      });
 
-    let revisionsToRemove = await revisions.reduce(reducer, Promise.resolve([]));
+    let revisionsToRemove = await revisions.reduce(
+      reducer,
+      Promise.resolve([]),
+    );
 
     // limit the number of concurrent requests
     const limit = pLimit(4);
 
     const concurrentDeleter = (revision) =>
-      limit((_) =>
-            collection.del({ name, revision }).then((_) => revision));
+      limit((_) => collection.del({ name, revision }).then((_) => revision));
 
-    return Promise.all(revisionsToRemove.map(concurrentDeleter)).then((revisions) => {
-      revisions = revisions.filter((r) => r);
-      if (revisions.length) {
-        if (opt.options.verbose) {
-          common.logWrite("deleted %s: %s", name, JSON.stringify(revisions));
+    return Promise.all(revisionsToRemove.map(concurrentDeleter)).then(
+      (revisions) => {
+        revisions = revisions.filter((r) => r);
+        if (revisions.length) {
+          if (opt.options.verbose) {
+            common.logWrite("deleted %s: %s", name, JSON.stringify(revisions));
+          }
+          return { item: name, revisions };
         }
-        return { item: name, revisions };
-      }
-      return null;
-    });
+        return null;
+      },
+    );
   }
   return null;
+}
+
+function validateCollection(v) {
+  const allowedValues = ["sharedflows", "proxies", "apiproxies"];
+
+  // First, check if the input string is one of the allowed values
+  if (!allowedValues.includes(v)) {
+    // If not, it fails the check, return an empty array or handle as an error
+    return null;
+  }
+
+  // Map "apiproxies" to "proxies", wrap in an array
+  return [v === "apiproxies" ? "proxies" : v];
 }
 
 common.verifyCommonRequiredParameters(opt.options, getopt);
 
 if (!opt.options.numToKeep) {
-  console.log("You must specify a number of revisions to retain. (-K)");
+  console.log(
+    "You must specify a number of revisions to retain. (-K) There is no default.",
+  );
+  getopt.showHelp();
+  process.exit(1);
+}
+
+const collectionNames = opt.options.collection
+  ? validateCollection(opt.options.collection)
+  : ["sharedflows", "proxies"];
+
+if (!collectionNames) {
+  console.log("You must specify a valid option for --collection");
   getopt.showHelp();
   process.exit(1);
 }
@@ -153,54 +185,67 @@ if (!opt.options.numToKeep) {
 apigee
   .connect(common.optToOptions(opt))
   .then((org) => {
-    let readOptions = {};
-    const collectionName = opt.options.sharedflows ? "sharedflows" : "proxies";
-    const collection = opt.options.sharedflows ? org.sharedflows : org.proxies;
-
-    return collection.get(readOptions).then((results) => {
-      if (results) {
-        // convert for GAAMBO
-        if (results.proxies && results.proxies.length) {
-          results = results.proxies.map((r) => r.name);
+    const readOptions = {};
+    const doOneCollection = (collectionName) => {
+      const collection = org[collectionName];
+      return collection.get(readOptions).then((results) => {
+        if (results) {
+          // convert for GAAMBO
+          if (results.proxies && results.proxies.length) {
+            results = results.proxies.map((r) => r.name);
+          }
+          if (results.sharedFlows && results.sharedFlows.length) {
+            results = results.sharedFlows.map((r) => r.name);
+          }
         }
-      }
-      if (opt.options.regexp) {
-        const re1 = new RegExp(opt.options.regexp);
-        results = results.filter((item) => re1.test(item));
-      }
-      if (!results || results.length == 0) {
-        common.logWrite(
-          "No %s%s",
-          opt.options.regexp ? "matching " : "",
-          collectionName
-        );
-        return Promise.resolve(true);
-      }
+        if (opt.options.regexp) {
+          const re1 = new RegExp(opt.options.regexp);
+          results = results.filter((item) => re1.test(item));
+        }
+        if (!results || results.length == 0) {
+          common.logWrite(
+            "No %s%s",
+            opt.options.regexp ? "matching " : "",
+            collectionName,
+          );
+          return Promise.resolve(true);
+        }
 
-      if (opt.options.verbose) {
-        console.log(JSON.stringify(results, null, 2));
-        common.logWrite(
-          "found %d %s%s",
-          results.length,
-          opt.options.regexp ? "matching " : "",
-          collectionName
-        );
-      }
-
-      const reducer = (promise, itemname) =>
-        promise.then((accumulator) =>
-          collection.getRevisions({ name: itemname }).then(async (r) => {
-            const x = await examineRevisions(collection, itemname, r);
-            return [...accumulator, x];
-          })
-        );
-
-      return results.reduce(reducer, Promise.resolve([])).then((a) => {
-        a = a.filter((x) => !!x);
         if (opt.options.verbose) {
-          common.logWrite("summary deleted: " + JSON.stringify(a));
+          console.log(JSON.stringify(results, null, 2));
+          common.logWrite(
+            "found %d %s%s",
+            results.length,
+            opt.options.regexp ? "matching " : "",
+            collectionName,
+          );
         }
+
+        const reducer = (promise, itemname) =>
+          promise.then((accumulator) =>
+            collection.getRevisions({ name: itemname }).then(async (r) => {
+              const x = await examineRevisions(collection, itemname, r);
+              accumulator.push(x);
+              return accumulator;
+            }),
+          );
+
+        return results.reduce(reducer, Promise.resolve([]));
       });
+    };
+
+    const r = (promise, item) =>
+      promise.then(async (accumulator) => {
+        const x = await doOneCollection(item);
+        accumulator.push(x);
+        return accumulator;
+      });
+
+    return collectionNames.reduce(r, Promise.resolve([])).then((a) => {
+      a = a.map((innerArray) => innerArray.filter((item) => item !== null));
+      if (opt.options.verbose) {
+        common.logWrite("summary deleted: " + JSON.stringify(a));
+      }
     });
   })
   .catch((e) => console.error("error: " + util.format(e)));
